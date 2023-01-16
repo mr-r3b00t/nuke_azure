f86d7a670218279a302076a68e60dadfd9c71bab
Daniel Card <dan.card@xservus.com>
2023-01-16 07:21:02 +0000

    Update nuke_azure.ps1
    added remove resource groups as well.
    This doesn't remove Recovery Services vaults (by nature of azure not by script logic)

--- nuke_azure.ps1
+++ nuke_azure.ps1
@@ -4,12 +4,26 @@ $azres = Get-AzResource
 ForEach($res in $azres)
 {
 
-if($res.ResourceGroupName -notlike '*shell*'){
+if($res.ResourceGroupName -notlike 'shell'){
 $res.ResourceGroupName
 write-host "Found resource $res.$res.ResourceGroupName for deletion" -foregroundcolor red
 write-host "Found resource $res.ResourceId for deletion" -foregroundcolor red
 
-Remove-AzResource -ResourceId $res.ResourceId -Force -Confirm:$False
+#Remove-AzResource -ResourceId $res.ResourceId -Force -Confirm:$False
+}
+
+}
+
+$azresgrp = Get-AzResourceGroup
+ForEach($resgrp in $azresgrp)
+{
+
+if($resgrp.ResourceGroupName -notlike 'shell'){
+$resgrp.ResourceGroupName
+write-host "Found resource $resgrp.$res.ResourceGroupName for deletion" -foregroundcolor red
+write-host "Found resource $resgrpId for deletion" -foregroundcolor red
+
+Remove-AzResourceGroup -ResourceId $resgrp.ResourceId -Force -Confirm:$False
 }
 
 }
